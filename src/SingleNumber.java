4028ab724c6170942245d861cf2d31670fea51b1
luoyangylh <longhuayou@gmail.com>
2014-04-26 23:01:00 -0500

    single number

---
+++ src/SingleNumber.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: longhuayou
+ * Date: 4/26/14
+ * Time: 10:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SingleNumber {
+    public int singleNumber(int[] A) {
+
+        int res = A[0];
+        for (int i = 0; i < A.length; i++) {
+            res ^= A[i];
+        }
+        return res;
+
+    }
+}
