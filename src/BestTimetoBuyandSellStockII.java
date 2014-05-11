73f800c10728cebc520b3b0e785da21930b3e736
luoyangylh <longhuayou@gmail.com>
2014-05-10 21:22:25 -0500

    unique binary tree

--- src/BestTimetoBuyandSellStockII.java
+++ src/BestTimetoBuyandSellStockII.java
@@ -6,5 +6,18 @@
  * To change this template use File | Settings | File Templates.
  */
 public class BestTimetoBuyandSellStockII {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        int len = prices.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            if (prices[i + 1] > prices[i]) {
+                res += prices[i+1] - prices[i];
+            }
+
+        }
+        return res;
+    }
+
 
 }
---
+++ src/UniqueBinarySearchTrees.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: longhuayou
+ * Date: 5/9/14
+ * Time: 10:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UniqueBinarySearchTrees {
+    public int numTrees(int n) {
+
+
+    }
+}
