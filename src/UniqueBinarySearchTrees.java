646c3eb92f9bc310d0e38cb955764f745b0202c1
luoyangylh <longhuayou@gmail.com>
2014-05-15 00:25:57 -0500

    unique tree

--- src/UniqueBinarySearchTrees.java
+++ src/UniqueBinarySearchTrees.java
@@ -7,7 +7,19 @@
  */
 public class UniqueBinarySearchTrees {
     public int numTrees(int n) {
+        int[] res = new int[n+1];
 
+        res[0] = 1;
+        res[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                res[i] += res[j] * res[i - j - 1];
+
+            }
+        }
+
+        return res[n];
 
     }
 }
